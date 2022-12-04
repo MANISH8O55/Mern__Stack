c9fafd9350e0aafc6e520009266e60059d3945d1
Manishb055 <manishmani00077@gmail.com>
2022-12-05 01:45:45 +0530

    2nd update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome To BUY-BRAND'S</h1>
+      <Header/>
+      <main className='py-2' align="center">
+        <Container>
+          <h1>Welcome To BUY-BRAND'S</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )    
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col classname='text-center py-3' align="center">Copyright &copy; BUY-BRAND'S</Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
